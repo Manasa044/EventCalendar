4eabe9761663d8cd7c98c31230367e387dbbcae8
Manasa <manasasambathina04@gmail.com>
2025-11-08 17:22:28 +0530

    Add dist folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Calender/', // 👈 your repo name on GitHub
+  base: '/EventCalendar/', // 👈 this must match your repo name
   plugins: [react()],
 })
