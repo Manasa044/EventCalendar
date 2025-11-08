d28a3b3eae17ceccc459687fe55f10cb1a3c3ea2
Manasa <manasasambathina04@gmail.com>
2025-11-08 17:36:56 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/EventCalendar/', // 👈 this must match your repo name
   plugins: [react()],
+  base: '/EventCalendar/', 
 })
